e2be67f9281e8a1b933175a08c61bcdf2918e985
Subham Shrestha <subhamshrestha@Subhams-MacBook-Pro.local>
2025-02-16 02:15:49 -0800

    partner block added

--- template-parts/block/hero-block.php
+++ template-parts/block/hero-block.php
@@ -1,11 +1,9 @@
 <?php /* Hero Block */ ?>
+<? if(get_field('hero_copy')):?>
 <div class="banner <?= pathinfo(__FILE__, PATHINFO_FILENAME);?>">
       <div class="container">
-        <h1>Your reliable Partner in the Trade Show Industry</h1>
-        <p>
-          Reliable solutions to elevate your trade show success. Let’s make your
-          events unforgettable.
-        </p>
+        <? the_field('hero_copy');?>
       </div>
     </div>
+<? endif;?>
 <!-- banner ends -->
